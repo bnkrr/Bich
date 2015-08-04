0cb195043ad3a9d386535a85bfa335ff66614976
chang dl <chdlgs@gmail.com>
2015-08-04 23:22:27 +0800

    核心event机制已实现

--- core.lua
+++ core.lua
@@ -1,8 +1,31 @@
 local addon, ns = ...
 local cfg = ns.cfg
 
-local initAddon()
+-- class(table) Bich
+Bich = {}
+
+function Bich:getCreatureIdByGuid(guid)
+    local utype, _, server, instance, zone, id, suid = strsplit("-", guid)
+    if utype == "Creature" then
+        return tonumber(id)
+    else
+        return -1
+    end
+end
+
+function Bich:getSpellIdByEvent()
+end
+
+function Bich:addSpell(id, spellid)
+end
+
+function Bich:getAllSpell()
+end
+
+
+local function initAddon()
 --初始化
+    DEFAULT_CHAT_FRAME:AddMessage("Bich reloaded", 255, 255, 255)
 end
 
 local function handlerAddonLoaded(name)
@@ -18,6 +41,13 @@ end
 local function handlerCombatLog(timeStamp, event, hideCaster, sourceGUID, sourceName, sourceFlags, 
                                 sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, ...)
 --存储新信息
+    if event == "SPELL_HEAL" or event == "SPELL_CAST_START" or event == "SPELL_CAST_SUCCESS" then
+        local spellid = select(1,...)
+        local unitid = Bich:getCreatureIdByGuid(sourceGUID)
+        if unitid ~= -1 then
+            DEFAULT_CHAT_FRAME:AddMessage(sourceName .. " spell: " .. GetSpellLink(spellid), 255, 255, 255)
+        end
+    end
 end
 
 local function onEvent(this, event, ...)
@@ -25,7 +55,7 @@ local function onEvent(this, event, ...)
         if event == "COMBAT_LOG_EVENT_UNFILTERED" then
             handlerCombatLog(...)
         elseif event == "PLAYER_TARGET_CHANGED" then
-            handlerTargetChanged(..)
+            handlerTargetChanged(...)
         elseif event == "ADDON_LOADED" then
             handlerAddonLoaded(...)
         end
